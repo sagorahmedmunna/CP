bcc2d46f2eff28318b482409da826b9da65b5c17
SAMee <sagorahmedmunna@gmail.com>
2023-10-16 21:37:45 +0600

    rename

---
+++ code templete/Binary_Exponentiation.cpp
@@ -0,0 +1,9 @@
+int BinExp(long long a, long long b, int mod) {
+    int ans = 1;
+    while (b) {
+        if (b & 1) ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return ans;
+}
