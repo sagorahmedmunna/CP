4f989d9365cc3ebaef5334055a675db8fd3dfcf7
SAMee <sagorahmedmunna@gmail.com>
2023-10-24 15:53:07 +0600

    Update _Custom Codes.cpp

--- code templete/_Custom Codes.cpp
+++ code templete/_Custom Codes.cpp
@@ -56,6 +56,7 @@ for (int i = 2; i <= N; i++) {
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
+template <typename num_t>
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 
 order_of_key();
