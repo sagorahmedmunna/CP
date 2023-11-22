b6582fc3fb0919a1033070cadf71e3ce914c1567
SAMee <sagorahmedmunna@gmail.com>
2023-11-22 17:32:55 +0600

    Update _Custom Codes.cpp

--- code templete/_Custom Codes.cpp
+++ code templete/_Custom Codes.cpp
@@ -76,8 +76,7 @@ struct custom_hash {
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-template <typename num_t>
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 order_of_key();
 find_by_order();
