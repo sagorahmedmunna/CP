d6aa35cb79bb2dba9f59a5877959a779b7985100
SAMee <sagorahmedmunna@gmail.com>
2024-02-26 20:12:43 +0600

    update sparse table

--- code templete/Sparse Table for minimums.cpp
+++ code templete/Sparse Table for minimums.cpp
@@ -1,3 +1,5 @@
+// 1 based indexing
+
 const int N = 2e5 + 10;
 const int LOG = 18;
 int table[N][LOG];
@@ -5,15 +7,15 @@ int a[N];
 int n;
 
 void sparse() {
-    for (int i = 0; i < n; i++) table[i][0] = a[i];
-    for (int j = 1; j < LOG; j++) {
-        for (int i = 0; i + (1 << j) - 1 < n; i++) {
-            table[i][j] = min(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
-        }
+  for (int i = 0; i < n; i++) table[i][0] = a[i];
+  for (int j = 1; j < LOG; j++) {
+    for (int i = 0; i + (1 << j) - 1 < n; i++) {
+      table[i][j] = min(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
     }
+  }
 }
 
 int query(int l, int r) {
-    int log = __lg(r - l + 1);
-    return min(table[l][log], table[r - (1 << log) + 1][log]);
+  int log = __lg(r - l + 1);
+  return min(table[l][log], table[r - (1 << log) + 1][log]);
 }
