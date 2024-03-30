8475506f0f2fa6dc80d5fcef8c0047cb7d2ab82e
SAMee <sagorahmedmunna@gmail.com>
2024-03-31 01:45:34 +0600

    update custom codes

--- code templete/_Custom Codes.cpp
+++ code templete/_Custom Codes.cpp
@@ -81,8 +81,13 @@ template<class T> using ordered_multiset = tree<T, null_type, greater_equal<T>, 
 template <typename T, typename R> using ordered_map = tree<T, R, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // Fucntions
-order_of_key(k): Gives the count of elements smaller than k. — O(log n)
-find_by_order(k): Returns the iterator for the kth element (use k = 0 for the first element). — O(log n)
+order_of_key(k): // Gives the count of elements smaller than k. — O(log n)
+find_by_order(k): // Returns the iterator for the kth element (use k = 0 for the first element). — O(log n)
 
 // To remove an element in a multiset, you must delete it using iterators:
 ss.erase(ss.find_by_order(ss.order_of_key(x)));
+
+// ordered_multiset erase
+int rank = st.order_of_key(val); //Number of elements that are less than val in st
+auto it = st.find_by_order(rank); // //Iterator that points to the (rank+1)th element in st
+st.erase(it);
