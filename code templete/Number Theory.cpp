17645cfd0d6712e6a5055726d353437dda6479c6
SAMee <sagorahmedmunna@gmail.com>
2024-04-19 03:03:30 +0600

    add number theory

---
+++ code templete/Number Theory.cpp
@@ -0,0 +1,109 @@
+const int N = 1e6 + 9;
+vector<int> lpf(N), gpf(N);
+map<int, int> factors[N];
+
+// sieve for finding lowest prime and highest prime upto N
+for (int i = 2; i < N; i++) {
+  if (lpf[i] == 0) {
+    for (int j = i; j < N; j += i) {
+      gpf[j] = i;
+      if (!lpf[j]) lpf[j] = i;
+    }
+  }
+}
+// find all factors upto N
+for (int i = 2; i < N; i++) {
+  int num = i;
+  while (num > 1) {
+    factors[i][lpf[num]]++;
+    num /= lpf[num];
+  }
+}
+int num = 10;
+int total_divisors = 1;
+long long sum_of_divisors = 1;
+for (auto& [p, c] : factors[num]) {
+  total_divisors *= (c + 1);
+  sum_of_divisors *= (pow(p, c + 1) - 1) / (p - 1);
+}
+
+
+// (a * b) % p   (Binary Multiplication)
+long long BinMul(long long a, long long b, long long mod) {
+  long long ans = 0;
+  while (b) {
+    if (b & 1) ans = (ans + a) % mod;
+    a = (a + a) % mod;
+    b >>= 1;
+  }
+  return ans;
+}
+// (a ^ b) % p   (Binary Exponentiation)
+long long BinExp(long long a, long long b, long long mod) {
+	long long ans = 1;
+	while (b) {
+		if (b & 1) ans = (BinMul(ans, a, mod)) % mod;
+		a = (BinMul(a, a, mod)) % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+// (a / b) % p
+BinMul(a, BinExp(b, p - 2, p), p);
+// (a ^ (b ^ c)) % p
+BinExp(a, BinExp(b, c, p - 1), p);
+
+// phi of single integer
+int n = 10;
+long long num = n;
+long long phi_of_n = n;
+for (long long i = 2; i * i <= num; i++) {
+	if (num % i == 0) {
+		while (num % i == 0) num /= i;
+		phi_of_n -= phi_of_n / i;
+	}
+}
+if (num > 1) phi_of_n -= phi_of_n / num;
+
+// phi upto N
+vector<int> phi(N);
+// initial 0 to N
+iota(phi.begin(), phi.end(), 0);
+for (int i = 2; i < N; i++) {
+  if (phi[i] == i) {
+    for (int j = i; j < N; j += i) {
+      phi[j] -= phi[j] / i;
+    }
+  }
+}
+
+// gcd sum -> ∑ gcd(i, n) for 1 <= i <= n; (n <= 1e9)
+// gcd(1, n) + gcd(2, n) + .. + gcd(n, n)
+long long sum = 0;
+for (int i = 1; 1LL * i * i <= n; i++) {
+	if (n % i == 0) {
+		sum += i * phi(n / i);
+		if (n / i != i) sum += (n / i) * phi(n / (n / i));
+	}
+}
+// all pair gcd sum from 1 to N (N <= 4e6)
+// ∑ i to n - 1, ∑ j + 1 to n [gcd(i, j)]
+for (int i = 1; i < N; i++) {
+	for (int j = i; j < N; j += i) sum[j] += i * phi[j / i];
+}
+for (int i = 1; i < N; i++) sum[i] += sum[i - 1] - i;
+
+
+// lcm sum -> ∑ lcm(i, n) for 1 <= i <= n; (n <= 1e6)
+// lcm(1, n) + lcm(2, n) + .. + lcm(n, n)
+phi[1] = 2; // phi[1] should be 2 for this algorithm
+for (int i = 1; i < N; i++) {
+	for (int j = i; j < N; j += i) lcm_sum[j] += 1LL * j * phi[i] * i / 2;
+}
+// all pair lcm sum from 1 to N (N <= 1e6)
+// ∑ i to n - 1, ∑ j + 1 to n [lcm(i, j)]
+phi[1] = 2; // phi[1] should be 2 for this algorithm
+for (int i = 1; i < N; i++) {
+	for (int j = i; j < N; j += i) lcm_sum[j] += (long long) j * (1LL * i * phi[i] / 2);
+}
+for (int i = 1; i < N; i++) lcm_sum[i] += lcm_sum[i - 1] - i;
