deab56c48e499e2f2e9cc5583a3a536fba6cbf72
SAMee <sagorahmedmunna@gmail.com>
2024-04-21 07:00:05 +0600

    update

--- code templete/Road Reparation (MST) with (DSU).cpp
+++ code templete/Road Reparation (MST) with (DSU).cpp
@@ -1,32 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 2e5 + 9;
-int parent[N], sz[N];
-
-void make(int u) {
-  parent[u] = u;
-  sz[u] = 1;
-}
-
-int find(int u) {
-  if (u == parent[u]) return u;
-  return parent[u] = find(parent[u]);
-}
-
-bool is_connected(int u, int v) {
-  return find(u) == find(v);
-}
-
-void Union(int u, int v) {
-  u = find(u);
-  v = find(v);
-  if (u != v) {
-    if (sz[u] < sz[v]) swap(u, v);
-    parent[v] = u;
-    sz[u] += sz[v];
+struct DSU {
+  int size = 1;
+  vector<int> parent, sz;
+  void init(int n) {
+    size = n;
+    parent.resize(n + 1);
+    sz.resize(n + 1);
+    for (int u = 1; u <= n; u++) {
+      parent[u] = u;
+      sz[u] = 1;
+    }
   }
-}
+  int find(int u) {
+    if (u == parent[u]) return u;
+    return parent[u] = find(parent[u]);
+  }
+  bool is_connected(int u, int v) {
+    return find(u) == find(v);
+  }
+  void Union(int u, int v) {
+    u = find(u);
+    v = find(v);
+    if (u != v) {
+      if (sz[u] < sz[v]) swap(u, v);
+      parent[v] = u;
+      sz[u] += sz[v];
+    }
+  }
+};
 
 int main() {
   ios_base::sync_with_stdio(0), cin.tie(0);
@@ -38,16 +41,18 @@ int main() {
     if (u > v) swap(u, v);
   }
   sort(edges.begin(), edges.end());
-  for (int i = 1; i <= n; i++) make(i);
+  
+  DSU d;
+  d.init(n);
   long long cost = 0;
   for (auto& [w, u, v] : edges) {
-    if (is_connected(u, v)) continue;
-    Union(u, v);
+    if (d.is_connected(u, v)) continue;
+    d.Union(u, v);
     cost = cost + w;
   }
   int cnt = 0;
   for (int i = 1; i <= n; i++) {
-    cnt += i == find(i);
+    cnt += i == d.find(i);
   }
   if (cnt > 1) {
     cout << "IMPOSSIBLE" << '\n';
