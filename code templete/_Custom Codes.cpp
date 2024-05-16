591ca2bf58c845d3eabcbf18023a31d156c1204a
SAMee <sagorahmedmunna@gmail.com>
2024-05-17 00:46:22 +0600

    update

--- code templete/_Custom Codes.cpp
+++ code templete/_Custom Codes.cpp
@@ -110,7 +110,7 @@ template <typename T, typename R> using ordered_map = tree<T, R, greater<T>, rb_
 
 // Fucntions
 order_of_key(k): // Gives the count of elements smaller than k. — O(log n)
-find_by_order(k): // Returns the iterator for the kth element (use k = 0 for the first element). — O(log n)
+find_by_order(k): // Returns the iterator for the kth element (0 based index). — O(log n)
 
 // To remove an element in a multiset, you must delete it using iterators:
 ss.erase(ss.find_by_order(ss.order_of_key(x)));
