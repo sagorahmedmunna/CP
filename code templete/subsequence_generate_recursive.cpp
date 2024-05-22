ee9392fa76efaefea97aba0efa686dd808b08714
SAMee <sagorahmedmunna@gmail.com>
2024-05-23 00:13:28 +0600

    update

--- code templete/subsequence_generate_recursive.cpp
+++ code templete/subsequence_generate_recursive.cpp
@@ -7,31 +7,31 @@ vector<vector<int>> sub_seq;
 int n;
 
 void rec(int p) {
-    if (p == n) {
-        vector<int> temp;
-        for (int i = 0; i < n; i++) {
-            if (is_taken[i]) temp.push_back(arr[i]);
-        }
-        sub_seq.push_back(temp);
-        return;
+  if (p == n) {
+    vector<int> temp;
+    for (int i = 0; i < n; i++) {
+      if (is_taken[i]) temp.push_back(arr[i]);
     }
+    sub_seq.push_back(temp);
+    return;
+  }
 
-    is_taken[p] = false; // do not take p'th position
-    rec(p + 1);
+  is_taken[p] = false; // do not take p'th position
+  rec(p + 1);
 
-    is_taken[p] = true; // take p'th position
-    rec(p + 1);
+  is_taken[p] = true; // take p'th position
+  rec(p + 1);
 }
 
 int main() {
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> arr[i];
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> arr[i];
 
-    rec(0);
+  rec(0);
 
-    for (auto& vec : sub_seq) {
-        for (auto& el : vec) cout << el << ' ';
-        cout << '\n';
-    }
-    return 0;
+  for (auto& vec : sub_seq) {
+    for (auto& el : vec) cout << el << ' ';
+    cout << '\n';
+  }
+  return 0;
 }
