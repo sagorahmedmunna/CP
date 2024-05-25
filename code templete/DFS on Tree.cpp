4817037349d688d9963198b9dcc064000bc0da30
SAMee <sagorahmedmunna@gmail.com>
2024-05-25 22:52:12 +0600

    edit

--- code templete/DFS on Tree.cpp
+++ code templete/DFS on Tree.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 const int N = 2e5 + 10, K = 20;
-vector<int> adj[N], tin(N), tout(N), dep(N), height(N), parent(N);
+vector<int> adj[N], tin(N), tout(N), depth(N), height(N), parent(N);
 vector<int> is_leaf(N, 1),subtree_size(N);
-int one_cut_subtree_size_difference = -1;
+int one_cut_subtree_size_difference = N;
 int t, n;
 
 void dfs(int u, int p) {
@@ -12,10 +12,10 @@ void dfs(int u, int p) {
 	parent[u] = p;
 	for (auto& v : adj[u]) {
 		if (v == p) continue;
-		dep[v] = dep[u] + 1;
+		depth[v] = depth[u] + 1;
 		is_leaf[u] = 0;
 		dfs(v, u);
-		height[v] = max(height[v], height[u] + 1);
+		height[u] = max(height[u], height[v] + 1);
 		subtree_size[u] += subtree_size[v] + 1;
 	}
 	one_cut_subtree_size_difference = min(abs(subtree_size[u] - (n - subtree_size[u])), one_cut_subtree_size_difference);
