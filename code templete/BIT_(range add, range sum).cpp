c58398ab4bc6a7ff343d38324ff842845260fd9d
SAMee <sagorahmedmunna@gmail.com>
2024-07-26 08:14:16 +0600

    add bit

---
+++ code templete/BIT_(range add, range sum).cpp
@@ -0,0 +1,83 @@
+// Range Update Queries
+// https://cses.fi/problemset/task/1651/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+struct BIT {
+  int size;
+  vector<long long> bit;
+  BIT() {}
+  BIT(int n) {
+    size = n;
+    bit.assign(n + 1, 0);
+  }
+  void update(int idx, long long val) {
+    while (idx <= size) {
+      bit[idx] += val;
+      idx += idx & -idx;
+    }
+  }
+  void update(int l, int r, long long val) {
+    update(l, val);
+    update(r + 1, -val);
+  }
+  long long query(int idx) {
+    long long res = 0;
+    while (idx > 0) {
+      res += bit[idx];
+      idx -= idx & -idx;
+    }
+    return res;
+  }
+  long long query(int l, int r) {
+    return query(r) - query(l - 1);
+  }
+};
+
+struct LazyBIT {
+  BIT bitAdd, bitSub;
+  LazyBIT(int n) : bitAdd(n), bitSub(n) {}
+  void update(int l, int r, int val) {
+    bitAdd.update(l, r, val);
+    bitSub.update(l, r, 1LL * (l - 1) * val);
+    bitSub.update(r + 1, 1LL * (-r + l - 1) * val);
+  }
+  long long query(int idx) {
+    return idx * bitAdd.query(idx) - bitSub.query(idx);
+  }
+  long long query(int l, int r) {
+    return query(r) - query(l - 1);
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0);
+  
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+
+  LazyBIT sg(n);
+  for (int i = 1; i <= n; i++) {
+    sg.update(i, i, a[i]);
+  }
+ 
+  while (q--) {
+    int type;
+    cin >> type;
+    if (type == 1) {
+      int l, r, val;
+      cin >> l >> r >> val;
+      sg.update(l, r, val);
+    } else {
+      int k;
+      cin >> k;
+      cout << sg.query(k, k) << '\n';
+    }
+  }
+  return 0;
+}
