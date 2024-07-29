8e1c8b552517c81105c72383362f30cc13be2e98
SAMee <sagorahmedmunna@gmail.com>
2024-07-30 00:25:42 +0600

    update

--- code templete/LCA (BINARY LIFTING).cpp
+++ code templete/LCA (BINARY LIFTING).cpp
@@ -77,16 +77,17 @@ int main() {
 
 // another approach
 // make u and v to same depth then lift
+// a little time consuming
 int lca(int u, int v) {
   if (depth[u] < depth[v]) swap(u, v);
-  int k = depth[u] - depth[v];
-  for (int i = K - 1; i >= 0; i--) {
-    if (k & (1 << i)) {
+  int dis = depth[u] - depth[v];
+  for (int i = k - 1; i >= 0; i--) {
+    if (dis & (1 << i)) {
       u = par[u][i];
     }
   }
   if (u == v) return u;
-  for (int i = K - 1; i >= 0; i--) {
+  for (int i = k - 1; i >= 0; i--) {
     if (par[u][i] != par[v][i]) {
       u = par[u][i];
       v = par[v][i];
