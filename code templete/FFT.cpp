f3da8983e2f2ae4a44ba0d2b0dfdef0e86f1b604
SAMee <sagorahmedmunna@gmail.com>
2024-08-10 09:48:21 +0600

    update

--- code templete/FFT.cpp
+++ code templete/FFT.cpp
@@ -4,35 +4,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const double PI = acos(-1);
 struct cplx {
-  double a, b;
-  cplx(double a = 0, double b = 0) : a(a), b(b) {}
+  long double a, b;
+  cplx(long double a = 0, long double b = 0) : a(a), b(b) {}
   const cplx operator + (const cplx &c) const { return cplx(a + c.a, b + c.b); }
   const cplx operator - (const cplx &c) const { return cplx(a - c.a, b - c.b); }
   const cplx operator * (const cplx &c) const { return cplx(a * c.a - b * c.b, a * c.b + b * c.a); }
-  const cplx operator / (const double &d) const { return cplx(a / d, b / d); }
+  const cplx operator / (const long double &d) const { return cplx(a / d, b / d); }
 };
 
-void fft(vector<cplx> &p, bool inv = 0) {
-  int n = p.size(), i = 0;
-  for (int j = 1; j < n - 1; ++j) {
-    for (int k = n >> 1; k > (i ^= k); k >>= 1);
-    if(j < i) swap(p[i], p[j]);
+const long double PI = acos(-1);
+vector<int> rev;
+
+void Preprocess(int sz){
+  if ((int)rev.size() == sz) return;
+  rev.resize(sz);
+  rev[0] = 0;
+  int lg_n = __builtin_ctz(sz);
+  for (int i = 1; i < sz; ++i) {
+    rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (lg_n - 1));
   }
-  for (int l = 1, m; (m = l << 1) <= n; l <<= 1) {
-    double ang = 2 * PI / m;
-    cplx wn = cplx(cos(ang), (inv ? 1. : -1.) * sin(ang)), w;
-    for (int i = 0, j, k; i < n; i += m) {
-      for (w = cplx(1, 0), j = i, k = i + l; j < k; ++j, w = w * wn) {
-        cplx t = w * p[j + l];
-        p[j + l] = p[j] - t;
-        p[j] = p[j] + t;
+}
+
+void fft(vector<cplx> &a, bool inv = 0){
+  int n = a.size();
+  for (int i = 1; i < n - 1; ++i) {
+    if (i < rev[i]) swap(a[i], a[rev[i]]);
+  }
+  for (int len = 2; len <= n; len <<= 1){
+    long double t = 2 * PI / len * (inv ? -1 : 1);
+    cplx wlen = {cosl(t), sinl(t)};
+    for (int st = 0; st < n; st += len){
+      cplx w(1);
+      for (int i = 0; i < len / 2; ++i){
+        cplx ev = a[st + i], od = a[st + i + len / 2] * w;
+        a[st + i] = ev + od;
+        a[st + i + len / 2] = ev - od;
+        w = w * wlen;
       }
     }
   }
   if (inv) {
-    for (int i = 0; i < n; ++i) p[i] = p[i] / n;
+    for (cplx& z : a) z = z / n;
   }
 }
 
@@ -44,11 +57,12 @@ vector<long long> multiply(vector<int> &a, vector<int> &b) {
     x[i] = i < n ? cplx(a[i], 0) : cplx(0, 0);
     y[i] = i < m ? cplx(b[i], 0) : cplx(0, 0);
   }
+  Preprocess(sz);
   fft(x), fft(y);
   for(int i = 0; i < sz; i++) z[i] = x[i] * y[i];
   fft(z, 1);
   vector<long long> c(n + m - 1);
-  for(int i = 0; i < n + m - 1; i++) c[i] = (long long)round(z[i].a);
+  for(int i = 0; i < n + m - 1; i++) c[i] = round(z[i].a);
   return c;
 }
 
