3ec9665f79661193540cd19f8b76eeacb47ce291
SAMee <sagorahmedmunna@gmail.com>
2024-08-15 14:24:57 +0600

    update

--- code templete/LCA.cpp
+++ code templete/LCA.cpp
@@ -3,20 +3,17 @@ using namespace std;
 
 struct DFS {
   int n, k, t = 0;
-  vector<int> tin, tout, depth, height, subtree_size;
-  vector<bool> is_leaf;
+  vector<int> tin, tout, depth;
   vector<vector<int>> parent;
   DFS() {}
   DFS(int root, vector<vector<int>>& adj) {
-    n = (int)adj.size() + 1;
-    k = __lg(n) + 1;
-    tin.assign(n, 0), tout.assign(n, 0), depth.assign(n, 0), height.assign(n, 0), subtree_size.assign(n, 0), is_leaf.assign(n, 1);
+    n = (int)adj.size() + 1, k = __lg(n) + 1;
+    tin.assign(n, 0), tout.assign(n, 0), depth.assign(n, 0);
     parent = vector<vector<int>> (n, vector<int> (k));
     dfs(root, root, adj);
   }
   void dfs(int u, int p, vector<vector<int>>& adj) {
     tin[u] = ++t;
-    subtree_size[u] = 1;
     parent[u][0] = p;
     for (int i = 1; i < k; i++) {
       if (parent[u][i - 1] != -1) parent[u][i] = parent[parent[u][i - 1]][i - 1];
@@ -25,25 +22,13 @@ struct DFS {
     for (auto& v : adj[u]) {
       if (v != p) {
         depth[v] = depth[u] + 1;
-        is_leaf[u] = 0;
         dfs(v, u, adj);
-        height[u] = max(height[u], height[v] + 1);
-        subtree_size[u] += subtree_size[v];
       }
     }
     tout[u] = ++t;
   }
   bool is_ancestor(int u, int v) {
     return tin[u] <= tin[v] && tout[v] <= tout[u];
-  }
-  int kth_parent(int u, int kth) {
-    for (int i = k - 1; i >= 0; i--) {
-      if (kth & (1 << i)) {
-        u = parent[u][i];
-        if (u == -1) return u;
-      }
-    }
-    return u;
   }
 };
 
@@ -68,14 +53,14 @@ int main() {
   int n, q;
   cin >> n >> q;
   vector<vector<int>> adj(n + 1, vector<int> ());
-  for (int i = 1; i <= n; i++) {
+  for (int i = 1; i < n; i++) {
     int u, v;
     cin >> u >> v;
     adj[u].push_back(v);
     adj[v].push_back(u);
   }
 
-  LCA tree(0, adj);
+  LCA tree(1, adj);
 
   while (q--) {
     int u, v;
