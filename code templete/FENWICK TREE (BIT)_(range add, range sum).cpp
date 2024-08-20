6dc7812f43e323d47477ec51d25456afbb39e69d
SAMee <sagorahmedmunna@gmail.com>
2024-08-20 20:21:01 +0600

    update

--- code templete/FENWICK TREE (BIT)_(range add, range sum).cpp
+++ code templete/FENWICK TREE (BIT)_(range add, range sum).cpp
@@ -4,63 +4,48 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-struct FenWick {
-  int size;
-  vector<long long> bit;
-  FenWick() {}
-  FenWick(int n) {
-    size = n;
-    bit.assign(n + 1, 0);
+struct FenWickTree {
+  int n;
+  vector<long long> ft;
+  FenWickTree() {}
+  FenWickTree(int _n) {
+    n = _n;
+    ft.assign(n + 1, 0);
   }
-  void add(int idx, long long val) {
-    while (idx <= size) {
-      bit[idx] += val;
-      idx += idx & -idx;
+  void Add(int idx, long long val) {
+    for (int i = idx; i <= n; i += i & -i) {
+      ft[i] += val;
     }
   }
-  void add(int l, int r, long long val) {
-    add(l, val);
-    add(r + 1, -val);
+  void Add(int l, int r, long long val) {
+    Add(l, val);
+    Add(r + 1, -val);
   }
-  long long query(int idx) {
+  long long Csum(int idx) {
     long long res = 0;
-    while (idx > 0) {
-      res += bit[idx];
-      idx -= idx & -idx;
+    for (int i = idx; i > 0; i -= i & -i) {
+      res += ft[i];
     }
     return res;
   }
-  long long query(int l, int r) {
-    return query(r) - query(l - 1);
-  }
-  int lower_bound(long long val) {
-    // return the index of lowerbound of prefix sum
-    int curr = 0;
-    long long sum = 0;
-    for (int i = __lg(size); i >= 0; i--) {
-      if (curr + (1 << i) > size) continue;
-      if (bit[curr + (1 << i)] + sum < val) {
-        curr += (1 << i);
-        sum += bit[curr];
-      }
-    }
-    return curr + 1;
+  long long Rsum(int l, int r) {
+    return Csum(r) - Csum(l - 1);
   }
 };
 
-struct FenWickLazy {
-  FenWick bitAdd, bitSub;
-  FenWickLazy(int n) : bitAdd(n), bitSub(n) {}
-  void add(int l, int r, int val) {
-    bitAdd.add(l, r, val);
-    bitSub.add(l, r, 1LL * (l - 1) * val);
-    bitSub.add(r + 1, 1LL * (-r + l - 1) * val);
+struct FenWickTreeLazy {
+  FenWickTree bitAdd, bitSub;
+  FenWickTreeLazy(int n) : bitAdd(n), bitSub(n) {}
+  void Add(int l, int r, int val) {
+    bitAdd.Add(l, r, val);
+    bitSub.Add(l, r, 1LL * (l - 1) * val);
+    bitSub.Add(r + 1, 1LL * (-r + l - 1) * val);
   }
-  long long query(int idx) {
-    return idx * bitAdd.query(idx) - bitSub.query(idx);
+  long long Query(int idx) {
+    return idx * bitAdd.Csum(idx) - bitSub.Csum(idx);
   }
-  long long query(int l, int r) {
-    return query(r) - query(l - 1);
+  long long Query(int l, int r) {
+    return Query(r) - Query(l - 1);
   }
 };
 
@@ -74,9 +59,9 @@ int main() {
     cin >> a[i];
   }
 
-  FenWickLazy sg(n);
+  FenWickTreeLazy sg(n);
   for (int i = 1; i <= n; i++) {
-    sg.add(i, i, a[i]);
+    sg.Add(i, i, a[i]);
   }
  
   while (q--) {
@@ -85,11 +70,11 @@ int main() {
     if (type == 1) {
       int l, r, val;
       cin >> l >> r >> val;
-      sg.add(l, r, val);
+      sg.Add(l, r, val);
     } else {
       int k;
       cin >> k;
-      cout << sg.query(k, k) << '\n';
+      cout << sg.Query(k, k) << '\n';
     }
   }
   return 0;
