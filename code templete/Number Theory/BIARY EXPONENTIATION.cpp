f4acb6a968c22a5af9915fb6cdb76851bcdcea5e
SAMee <sagorahmedmunna@gmail.com>
2024-09-02 19:19:33 +0600

    update

--- code templete/Number Theory/BIARY EXPONENTIATION.cpp
+++
@@ -1,9 +0,0 @@
-int BinExp(long long a, long long b, int mod) {
-	int res = 1;
-	while (b) {
-		if (b & 1) res = (res * a) % mod;
-		a = (a * a) % mod;
-		b >>= 1;
-	}
-	return res;
-}
--- code templete/Number Theory/BINARY MULTIPLICATION.cpp
+++
@@ -1,9 +0,0 @@
-int BinMul(long long a, long long b, long long mod) {
-	int res = 0;
-	while (b) {
-		if (b & 1) res = (res + a) % mod;
-		a = (a + a) % mod;
-		b >>= 1;
-	}
-	return res;
-}
