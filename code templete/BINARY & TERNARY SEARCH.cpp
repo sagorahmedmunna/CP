7bfc4eda77f1ed3da0809a1f3d7f838229a8a080
SAMee <sagorahmedmunna@gmail.com>
2024-09-05 21:03:51 +0600

    update

--- code templete/BINARY & TERNARY SEARCH.cpp
+++ code templete/BINARY & TERNARY SEARCH.cpp
@@ -21,7 +21,7 @@ while (lo < hi) {
   // int mid1 = lo + (hi - lo) / 3;
   // int mid2 = hi - (hi - lo) / 3;
   int mid = (lo + hi) / 2;
-  if (f(mid) > f(mid + 1)) lo = mid + 1;
+  if (f(mid) < f(mid + 1)) lo = mid + 1;
   else  hi = mid;
 }
 
@@ -31,6 +31,6 @@ int itr = 50;
 while (itr--) {
   double mid1 = lo + (hi - lo) / 3;
   double mid2 = hi - (hi - lo) / 3;
-  if (f(mid1) > f(mid2)) lo = mid1;
+  if (f(mid1) < f(mid2)) lo = mid1;
   else  hi = mid2;
 }
