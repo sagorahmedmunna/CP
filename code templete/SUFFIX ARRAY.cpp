de7e584831c892f089a118d7e2b72973c7714f39
SAMee <sagorahmedmunna@gmail.com>
2024-09-07 22:48:12 +0600

    update

--- code templete/SUFFIX ARRAY.cpp
+++ code templete/SUFFIX ARRAY.cpp
@@ -13,8 +13,8 @@ array<vector<int>, 2> get_sa(string& s, int lim = 128) {
     for (int i = 1; i < lim; ++i) ws[i] += ws[i - 1];
     for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
     swap(x, y), p = 1, x[sa[0]] = 0;
-    for (int i = 1; i < n; ++i) a = sa[i - 1], b = sa[i], x[b] =
-      (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+    for (int i = 1; i < n; ++i) a = sa[i - 1], b = sa[i], 
+      x[b] = (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
   }
   for (int i = 1; i < n; ++i) rank[sa[i]] = i;
   for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
