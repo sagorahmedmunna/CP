786e952330ea37951dc4a71684dd89de52fbb6e2
SAMee <sagorahmedmunna@gmail.com>
2024-09-23 01:28:47 +0600

    update

--- Stress Testing/GENERATE.cpp
+++ Stress Testing/GENERATE.cpp
@@ -14,4 +14,26 @@ int main() {
     int n = rnd(1, 99999);
     cout << n << ' ';
   }
+
+  // generate tree
+  // int n = rnd(2, 20);
+  // printf("%d\n", n);
+  // vector<pair<int,int>> edges;
+  // for(int i = 2; i <= n; ++i) {
+  //   edges.emplace_back(rnd(1, i - 1), i);
+  // }
+  // vector<int> perm(n + 1); // re-naming vertices
+  // for(int i = 1; i <= n; ++i) {
+  //   perm[i] = i;
+  // }
+  // random_shuffle(perm.begin() + 1, perm.end());
+  // random_shuffle(edges.begin(), edges.end()); // random order of edges
+  // for(pair<int, int> edge : edges) {
+  //   int a = edge.first, b = edge.second;
+  //   if(rng() % 2) {
+  //     swap(a, b); // random order of two vertices
+  //   }
+  //   printf("%d %d\n", perm[a], perm[b]);
+  // }
+  // make sure to check bambo tree and star tree
 }
