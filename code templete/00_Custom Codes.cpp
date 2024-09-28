f91857ca8b315216b073777b799e3b928acd2344
SAMee <sagorahmedmunna@gmail.com>
2024-09-29 01:37:36 +0600

    update

--- code templete/00_Custom Codes.cpp
+++ code templete/00_Custom Codes.cpp
@@ -97,26 +97,3 @@ __builtin_ctzll(x)
 bool isInside(int x1, int y1, int x2, int y2, int r) {
 	return ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) <= r * r;
 }
-
----------------------------------------------------------
-// gp hash table
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-gp_hash_table<int, int> mp;
-
----------------------------------------------------------
-// unordered map custom hash
-
-struct custom_hash {
-	static uint64_t splitmix64(uint64_t x) {
-		x += 0x9e3779b97f4a7c15;
-		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-		return x ^ (x >> 31);
-	}
-	size_t operator()(uint64_t x) const {
-		static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-		return splitmix64(x + FIXED_RANDOM);
-	}
-};
-unordered_map<int, int, custom_hash> ump;
