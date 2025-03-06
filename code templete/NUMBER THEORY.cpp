f0fab0cf133f88e28bde12f5a693ac885fdc396e
SAMee <sagorahmedmunna@gmail.com>
2025-03-07 00:57:24 +0600

    update

--- code templete/NUMBER THEORY.cpp
+++ code templete/NUMBER THEORY.cpp
@@ -63,15 +63,9 @@ struct Combinatorics {
   Combinatorics(int n) {
     fact.assign(n + 1, 1), inv.assign(n + 1, 1), ifact.assign(n + 1, 1);
     inv[0] = 0;
-    for (int i = 2; i <= n; i++) {
-      fact[i] = (fact[i - 1] * i) % mod;
-    }
-    for (int i = 2; i <= n; i++) {
-      inv[i] = mod - (mod / i) * inv[mod % i] % mod;
-    }
-    for (int i = 2; i <= n; i++) {
-      ifact[i] = (ifact[i - 1] * inv[i]) % mod;
-    }
+    for (int i = 2; i <= n; i++) fact[i] = (fact[i - 1] * i) % mod;
+    for (int i = 2; i <= n; i++) inv[i] = mod - (mod / i) * inv[mod % i] % mod;
+    for (int i = 2; i <= n; i++) ifact[i] = (ifact[i - 1] * inv[i]) % mod;
   }
   int nPr(int n, int r) { // Permutations
     if (n < r) return 0;
