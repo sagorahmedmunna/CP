7c8c4c8b3c89bbcce220d7e59402ff9001251752
Sagor Ahmed Munna <sagorahmedmunna@gmail.com>
2025-04-12 01:58:31 +0600

    update

--- CODE TEMPLATES/TRIE.cpp
+++ CODE TEMPLATES/TRIE.cpp
@@ -57,15 +57,16 @@ struct Trie {
 
 // iterative
 const int N = 1e6 + 9, A = 26;
-int nxt[N][A], cnt[N], node = 2;
+int nxt[N][A], pref_cnt[N], word_cnt[N], node = 2;
 void add(string& s) {
   int u = 1;
   for (auto& c : s) {
     int idx = c - 'a';
     if (!nxt[u][idx]) nxt[u][idx] = node++;
     u = nxt[u][idx];
-    cnt[u]++;
+    pref_cnt[u]++;
   }
+  word_cnt[u]++;
 }
 int cnt_pref(string& s) {
   int u = 1;
@@ -74,15 +75,24 @@ int cnt_pref(string& s) {
     if (!nxt[u][idx]) return 0;
     u = nxt[u][idx];
   }
-  return cnt[u];
+  return pref_cnt[u];
+}
+int find(string& s) {
+  int u = 1;
+  for (auto& c : s) {
+    int idx = c - 'a';
+    if (!nxt[u][idx]) return 0;
+    u = nxt[u][idx];
+  }
+  return word_cnt[u];
 }
 void erase(string& s) {
-  if (cnt_pref(s) == 0) return;
+  if (find(s) == 0) return;
   int u = 1;
   for (auto& c : s) {
     int idx = c - 'a';
     int v = nxt[u][idx];
-    if(--cnt[v] == 0) {
+    if(--pref_cnt[v] == 0) {
       nxt[u][idx] = 0 ;
       return;
     }
@@ -91,33 +101,34 @@ void erase(string& s) {
 }
 
 // trie integer iterative (max xor)
-const int N = 7e6 + 9, A = 2;
-int nxt[N][A], cnt[N], node = 2;
+const int N = 2e5 * 31 + 9, A = 2;
+int nxt[N][A], pref_cnt[N], int_cnt[N], node = 2;
 void add_int(int num) {
   int u = 1;
   for (int bit = 30; bit >= 0; bit--) {
     int idx = (num >> bit) & 1;
     if (!nxt[u][idx]) nxt[u][idx] = node++;
     u = nxt[u][idx];
-    cnt[u]++;
+    pref_cnt[u]++;
   }
+  int_cnt[u]++;
 }
-int int_cnt(int num) {
+int find(int num) {
   int u = 1;
   for (int bit = 30; bit >= 0; bit--) {
     int idx = (num >> bit) & 1;
     if (!nxt[u][idx]) return 0;
     u = nxt[u][idx];
   }
-  return cnt[u];
+  return int_cnt[u];
 }
 void erase_int(int num) {
-  if (int_cnt(num) == 0) return;
+  if (find(num) == 0) return;
   int u = 1;
   for(int bit = 30; bit >= 0; bit--) {
     int idx = (num >> bit) & 1;
     int v = nxt[u][idx];
-    if(--cnt[v] == 0) {
+    if(--pref_cnt[v] == 0) {
       nxt[u][idx] = 0 ;
       return;
     }
@@ -129,7 +140,7 @@ int max_xor(int num) {
   for (int bit = 30; bit >= 0; bit--) {
     int idx = (num >> bit) & 1;
     int flip = idx ^ 1;
-    if (nxt[u][flip] && cnt[nxt[u][flip]]) {
+    if (nxt[u][flip] && pref_cnt[nxt[u][flip]]) {
       res += (1 << bit);
       u = nxt[u][flip];
     } else {
