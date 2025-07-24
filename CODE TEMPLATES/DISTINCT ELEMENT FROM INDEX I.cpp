8453ff7eeccdd11f3f89541ca6dc6a417d5e7422
Sagor Ahmed Munna <sagorahmedmunna@gmail.com>
2025-07-24 23:26:59 +0600

    update

--- CODE TEMPLATES/DISTINCT ELEMENT FROM INDEX I.cpp
+++ CODE TEMPLATES/DISTINCT ELEMENT FROM INDEX I.cpp
@@ -1,18 +1,18 @@
 // 0 based
 // count of distinct element from index i to left and right
 array<vector<int>, 2> distinctElementFromI(vector<int>& a) {
-  int n = a.size(), mxidx = -1;
+  int n = a.size(), mxIdx = -1;
   map<int, int> mp;
   vector<int> l(n);
   for (int i = 0; i < n; i++) {
-    if (mp.count(a[i])) mxidx = max(mxidx, mp[a[i]]);
-    mp[a[i]] = i, l[i] = i - mxidx;
+    if (mp.count(a[i])) mxIdx = max(mxIdx, mp[a[i]]);
+    mp[a[i]] = i, l[i] = i - mxIdx;
   }
-  mp.clear(), mxidx = n;
+  mp.clear(), mxIdx = n;
   vector<int> r(n);
   for (int i = n - 1; i >= 0; i--) {
-    if (mp.count(a[i])) mxidx = min(mxidx, mp[a[i]]);
-    mp[a[i]] = i, r[i] = mxidx - i;
+    if (mp.count(a[i])) mxIdx = min(mxIdx, mp[a[i]]);
+    mp[a[i]] = i, r[i] = mxIdx - i;
   }
   return {l, r};
 }
