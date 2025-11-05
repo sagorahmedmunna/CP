f804146bb9cb0fd4812add59c8cc41296fa6d6ea
Sagor Ahmed Munna <sagorahmedmunna@gmail.com>
2025-11-05 09:40:57 +0600

    update

--- CODE TEMPLATES/MANACHER.cpp
+++ CODE TEMPLATES/MANACHER.cpp
@@ -1,12 +1,7 @@
 struct Manacher { // 0 based
-  int n;
-  vector<int> p;
+  int n; vector<int> p;
   Manacher(string& s) {
-    n = s.size();
-    p.resize(2 * n);
-    build(s);
-  }
-  void build(string& s) {
+    n = s.size(); p.resize(2 * n);
     for (int i = 0, j = 0, k; i < n * 2; i += k, j = max(j - k, 0)) {
       while (i >= j && i + j + 1 < n * 2 && s[(i - j) / 2] == s[(i + j + 1) / 2]) ++j;
       p[i] = j;
@@ -21,8 +16,32 @@ struct Manacher { // 0 based
     else idx = (l + len / 2 - 1) * 2 + 1;
     return p[idx] >= len;
   }
-  int odd_length_of_center_i(int i) {return p[i * 2]; }
+  int odd_length_of_center_i(int i) { return p[i * 2]; }
   int even_length_of_center_i(int i) { return p[i * 2 + 1]; }
+  vector<int> mxR, mxL;
+  void longestPalindromeEndingAtEachPosition() {
+    mxR.assign(n, 1);
+    for (int i = 0; i < n; i++) {
+      int l = p[i * 2];
+      mxR[i + l / 2] = max(mxR[i + l / 2], l);
+      l = p[i * 2 + 1];
+      mxR[i + l / 2] = max(mxR[i + l / 2], l);
+    }
+    for (int i = n - 2; i >= 0; i--)
+      mxR[i] = max(mxR[i], mxR[i + 1] - 2);
+  }
+  void longestPalindromeStartingAtEachPosition() {
+    mxL.assign(n, 1); 
+    for (int i = n - 1; i >= 0; i--) { 
+      int l = p[i * 2];
+      mxL[i - l / 2] = max(mxL[i - l / 2], l);
+      l = p[i * 2 + 1];
+      int idx = min(n - 1, i - l / 2 + 1);
+      mxL[idx] = max(mxL[idx], l);
+    }
+    for (int i = 1; i < n; i++)
+      mxL[i] = max(mxL[i], mxL[i - 1] - 2); 
+  }
 };
 
 // p[0][i] = half length of longest even palindrome around pos i-1, i and starts at i-p[0][i] and ends at i+p[0][i]-1
