bd1ee16ee2ea4f6296153083d3938dbc7a1f8156
Sagor Ahmed Munna <sagorahmedmunna@gmail.com>
2026-01-18 12:18:25 +0600

    update

--- Codes/Prime_number_check.cpp
+++ Codes/Prime_number_check.cpp
@@ -1,8 +1,14 @@
-bool prime(int64_t n) {
-  if (n == 2) return 1;
-  if (n <= 1 || n % 2 == 0) return 0;
-  for (int i = 3; i * 1LL * i <= n; i += 2) {
-    if (n % i == 0) return 0;
+/*
+  Time: O(√n / 6)
+  Space: O(1)
+  n <= 10 ^ 12
+*/
+bool isPrime(int64_t n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 == 0 || n % 3 == 0) return false;
+  for (int64_t i = 5; i * i <= n; i += 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false;
   }
-  return 1;
+  return true;
 }
