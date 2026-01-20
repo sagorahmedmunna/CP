1b678d75d1a0245d510bbb8f977a2324364cf3d1
Sagor Ahmed Munna <sagorahmedmunna@gmail.com>
2026-01-21 00:12:40 +0600

    update

--- math fomula/math_formula.cpp
+++ math fomula/math_formula.cpp
@@ -1,5 +1,24 @@
-1 + 2 + 3 + ... + n = (n * (n + 1)) / 2
+1) Sum of first n natural numbers
+   1 + 2 + ... + n = n(n + 1) / 2
 
-1^2 + 2^2 + 3^2 + ... + n^2 = (n * (n + 1) * (2 * n + 1)) / 6
+2) Sum of squares
+   1^2 + 2^2 + ... + n^2 = n(n + 1)(2n + 1) / 6
 
-x^0 + x^1 + x^2 + ... + x^n = (x^(n + 1) - 1) / (x - 1)
+3) Sum of cubes
+   1^3 + 2^3 + ... + n^3 = (n(n + 1) / 2)^2
+
+4) Arithmetic Progression (AP)
+   a + (a + d) + ... + (a + (n - 1)d)
+   = n / 2 * (2a + (n - 1)d)
+
+5) Geometric Progression (GP)
+   1 + r + r^2 + ... + r^n
+   = (r^(n + 1) - 1) / (r - 1),  r != 1
+
+6) Infinite Geometric Progression (|r| < 1)
+   1 + r + r^2 + ...
+   = 1 / (1 - r)
+
+7) Quadratic Formula
+   ax^2 + bx + c = 0
+   x = (-b ± sqrt(b^2 - 4ac)) / (2a)
